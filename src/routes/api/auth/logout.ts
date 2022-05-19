cea2be240fb4ebdfbfcaf44d1a25a3d31ac46db6
Shindou Mihou <kokocrunchsh@gmail.com>
2022-05-19 09:57:44 +0800

    Add logout route and rename `/auth/new` route

---
+++ src/routes/api/auth/logout.ts
@@ -0,0 +1,8 @@
+export async function get() {
+    return {
+        status: 204,
+        headers: {
+            'set-cookie': `session=; Path=/; HttpOnly; SameSite=Lax; Expires=-1`
+        }
+    }
+}
