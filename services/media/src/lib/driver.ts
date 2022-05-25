8ef9b6d378cca2d7177364377f6921f814a10b1e
Miu <kokocrunchsh@gmail.com>
2022-05-26 07:09:10 +0800

    Delete services/media directory

--- services/media/src/lib/driver.ts
+++
@@ -1,6 +0,0 @@
-import { FileRequest } from "./models/file_request";
-
-export interface StorageDriver {
-    put: (request: FileRequest,  buffer: Buffer) => Promise<void>,
-    delete: (request: FileRequest) => Promise<void>
-}
